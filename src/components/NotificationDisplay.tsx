a7ed029d129d9213d93e45dc6e65783a53c45f32
Tobias Büchel <t.buechel@syon-group.com>
2024-05-27 22:57:44 +0200

    Add header to notification display

--- src/components/NotificationDisplay.tsx
+++ src/components/NotificationDisplay.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react'
 import OneSignal from 'react-onesignal'
+import classes from './NotificationDisplay.module.css'
 
 export const NotificationDisplay = () => {
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState<string | null>(null)
 
   useEffect(() => {
     OneSignal.Notifications.addEventListener('click', ({ notification }) => {
@@ -10,5 +11,12 @@ export const NotificationDisplay = () => {
     })
   })
 
-  return message && <div>{message}</div>
+  return (
+    message && (
+      <div className={classes.container}>
+        <h2>Notification Received</h2>
+        <div>{message}</div>
+      </div>
+    )
+  )
 }
