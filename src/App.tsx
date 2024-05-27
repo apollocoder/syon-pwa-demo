bcb31fc153af919171b6080a3d5a2460c5d963c8
Tobias Büchel <t.buechel@syon-group.com>
2024-05-27 17:37:39 +0200

    Disable VitePWA

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import { useEffect } from 'react'
 import classes from './App.module.css'
 import { PaymentButton } from './components/ShareButton'
-import { WebPushClient } from '@magicbell/webpush'
-
-const client = new WebPushClient({
-  apiKey: '6eed9cd644cad6f5cc09405ef676fcda6c188561',
-  userEmail: 'tobi.buechel@gmail.com',
-  serviceWorkerPath: '/sw-mb.js'
-})
 
 function App() {
-  useEffect(() => {
-    if (!client.isSubscribed()) client.subscribe()
-  }, [])
-
   return (
     <div className={classes.app}>
       <img src="/syon.png" alt="SYON" className={classes.logo} />
